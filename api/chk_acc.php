1739f818b69e3d7a6cae7931628f409a4040d1c5
s8140156 <s8140156@gmail.com>
2024-01-01 01:20:29 +0800

    順使用ajax方式註冊帳密確認邏輯及api串接

--- api/chk_acc.php
+++ api/chk_acc.php
@@ -5,7 +5,7 @@ include_once "db.php";
 $res=$User->count(['acc'=>$_POST['acc']]);
 
 if($res>0){
-	echo 1; //資料重覆了
+	echo 1; //1代表資料存在 所以資料重覆了
 }else{
 	echo 0;
 }
--- front/reg.php
+++ front/reg.php
@@ -20,6 +20,8 @@
 		</tr>
 		<tr>
 			<td><input type="button" value="註冊" onclick="reg()">
+			<!-- 在button“註冊(on＋click)”一個事件 使用reg() -->
+			<!-- type要使用button, 才可以onclick事件 submit是傳送表單 -->
 				<input type="reset" value="清除">
 			</td>
 			<td></td>
@@ -36,15 +38,20 @@
 			email: $("#email").val()
 			//這邊是先把表單上的資訊收集成一個js物件陣列(json) 在打包去後端
 		}
+		//邏輯順序：先判斷表單輸入是否有空白=>2次密碼輸入檢查(只會在前端發生 後端不會執行)=>帳號是否有重複
 		if (user.acc != '' && user.pw != '' && user.pw2 != '' && user.email != '') {
 			if (user.pw == user.pw2) {
 				$.post("./api/chk_acc.php",{acc:user.acc},(res)=>{
+					//這邊只傳{acc:user.acc}的資料過去 因為只是確認帳號是否有重複 要等確認帳號沒有重複後 才整包資料給資料庫
 					// console.log(res)
 					if(parseInt(res)==1){
+						// parseInt字串轉數字
+						// 判斷如果資料庫回傳是1(對資料庫count)代表有帳號並有重複則顯示alert
 						alert("帳號重覆")
 					}else{
 						$.post('./api/reg.php',user,(res)=>{
 							//實務上這邊應該要加判斷 if(res==1){}..
+							//經以上全部判斷後 才整包user(物件 所以是陣列格式)給api去資料庫寫進執行
 							alert('註冊完成 歡迎加入')
 						})
 
@@ -57,6 +64,8 @@
 			}
 		} else {
 			alert("不可空白")
+			//有發現如果當判斷式是以(!以否定)不是什麼的時候開始 其實是代表如果不是這樣的情況 就會執行什麼什麼 所以後面的執行會很多
+			//所以感覺要先把else後面的動作先寫好(就是如果是否定的話) 這樣如果裡面還有很幾層判斷式 這樣也比較清楚這層的結構在哪
 
 		}
 		// console.log(user);
