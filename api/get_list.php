d30f2e0581577c74e39f8c174379efb096a0addd
s8140156 <s8140156@gmail.com>
2024-01-06 01:40:39 +0800

    理解分類網誌使用ajax取得及載入資料庫文章列表邏輯

--- api/get_list.php
+++ api/get_list.php
@@ -2,12 +2,16 @@
 
 include_once "db.php";
 
+// $_GET['type'];
 
 $rows=$News->all(['type'=>$_GET['type'],'sh'=>1]);
 foreach($rows as $row){
 	echo "<a href='Javascript:getNews({$row['id']})' style='display:block'>";
+	// 取得文章列表要使用id(唯一值); 原本文章列表顯示無斷行 使用display:block並排
 	echo $row['title'];
 	echo "</a>";
 }
+
+//如何判斷取的資料是用ajax=>去F12 network確認是否有type:xhr 並重整畫面是不會跳的
 
 ?>
