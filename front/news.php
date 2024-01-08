37c21ad2db31fccb78be8fbd32fd15533110abda
s8140156 <s8140156@gmail>
2024-01-08 10:43:34 +0800

    增front/news點選標題後從短文章->顯示全部by jq onclick

--- front/news.php
+++ front/news.php
@@ -7,18 +7,60 @@
 			<th></th>
 		</tr>
 		<?php
-		$rows=$News->all(['sh'=>1]);
-		foreach($rows as $row){
+		// 在寫分頁時,注意擺放的位置, 原先從資料庫取資料出來的上方, 並這樣在取資料 會直接跟分頁合併取條件
+		$total = $News->count(['sh' => 1]);
+		$div = 5;
+		$pages = ceil($total / $div);
+		$now = $_GET['p'] ?? 1; //現在在的頁數
+		$start = ($now - 1) * $div; //從哪筆資料開始
 
-			
-			?>
-		<tr>
-			<td><?=$row['title'];?></td>
-			<td><?=mb_substr($row['news'],0,25);?></td>
-			<td></td>
-		</tr>
+		$rows = $News->all(['sh' => 1], " limit $start,$div"); //這邊與分頁取資料的篩選合併
+		foreach ($rows as $row) {
+		?>
+			<tr>
+				<td>
+					<div class="title" data-id="<?= $row['id']; ?>" style="cursor:pointer">
+						<?= $row['title']; ?></div>
+				</td>
+
+				<td>
+					<div id="s<?=$row['id'];?>"><?= mb_substr($row['news'], 0, 25); ?>...</div>
+					<div id="a<?=$row['id'];?>" style="display:none"><?=$row['news'];?></div>
+				</td>
+				<td></td>
+			</tr>
 		<?php
 		}
+
 		?>
 	</table>
+	<div>
+		<?php
+		if (($now - 1) > 0) {
+			$prev = $now - 1;
+			echo "<a href='?do=news&p=$prev'> < </a>";
+		}
+
+		for ($i = 1; $i <= $pages; $i++) {
+			$fontsize = ($now == $i) ? "font-size:20px" : "font-size:16px";
+			echo "<a href='?do=news&p=$i' style='$fontsize'> $i </a>";
+		}
+		if (($now + 1) <= $pages) {
+			$next = $now + 1;
+			echo "<a href='?do=news&p=$next'> > </a>";
+		}
+		?>
+	</div>
+	<!-- 注意這邊用div包起來用於顯示 -->
 </fieldset>
+
+<script>
+$(".title").on('click',(e)=>{
+    let id=$(e.target).data('id');
+    $(`#s${id},#a${id}`).toggle();
+    //$("#s"+id+",#a"+id).toggle();
+
+})
+
+
+</script>
--- front/pop.php
+++ front/pop.php
@@ -7,20 +7,55 @@
 			<th width="">人氣</th>
 		</tr>
 		<?php
-		$rows=$News->all(['sh'=>1],"order by `good` desc");
-		// 人氣文章要注意加排序 讚數多->少
-		// 請注意陣列的括號(一組一組)
-		foreach($rows as $row){
+		// 在寫分頁時,注意擺放的位置, 原先從資料庫取資料出來的上方, 並這樣在取資料 會直接跟分頁合併取條件
+		$total = $News->count(['sh' => 1]);
+		$div = 5;
+		$pages = ceil($total / $div);
+		$now = $_GET['p'] ?? 1; //現在在的頁數
+		$start = ($now - 1) * $div; //從哪筆資料開始
 
-			
-			?>
-		<tr>
-			<td><?=$row['title'];?></td>
-			<td><?=mb_substr($row['news'],0,25);?>...</td>
-			<td></td>
-		</tr>
+		$rows = $News->all(['sh' => 1], " order by `good` desc limit $start,$div"); //要注意寫法要先寫order->desc->最後才寫limit
+		foreach ($rows as $row) {
+		?>
+			<tr>
+				<td>
+					</div><?= $row['title']; ?><div>
+				</td>
+				<td><div></div><?= mb_substr($row['news'], 0, 25); ?></td>
+				<td></td>
+			</tr>
 		<?php
 		}
+
 		?>
 	</table>
+	<div>
+		<?php
+		if (($now - 1) > 0) {
+			$prev = $now - 1;
+			echo "<a href='?do=news&p=$prev'> < </a>";
+		}
+
+		for ($i = 1; $i <= $pages; $i++) {
+			$fontsize = ($now == $i) ? "font-size:20px" : "font-size:16px";
+			echo "<a href='?do=news&p=$i' style='$fontsize'> $i </a>";
+		}
+		if (($now + 1) <= $pages) {
+			$next = $now + 1;
+			echo "<a href='?do=news&p=$next'> > </a>";
+		}
+		?>
+	</div>
+	<!-- 注意這邊用div包起來用於顯示 -->
 </fieldset>
+
+<script>
+	$(".title").on('click',(e)=>{
+		let id=$(e.target).data('id');
+		$(`#s+${id},#a+${id}`).toggle();
+		// 注意上引號要整個包起來 , 算進字串裡
+		// $("#s"+id+",#a"+id).toggle();
+
+	})
+
+</script>
